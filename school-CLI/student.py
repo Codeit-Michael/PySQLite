671745ae82b6c45324b5fff92b5f7458cf01a8f1
Michael Maranan <michaelmaranan030@gmail.com>
2022-03-29 23:20:04 +0800

    fixed student and course relationship

--- school-CLI/student.py
+++ school-CLI/student.py
@@ -15,6 +15,11 @@ class Student():
 		self.address = address
 		self.year = year
 		self.course = course
+		course_id = f'{course}/yr-{yr}'
+		course_tuple = (self.course,)
+
+		if course_tuple not in self.cursr.execute("SELECT name FROM courses"):
+			return print('Error 404')
 
 		self.cursr.execute("INSERT INTO students (?,?,?,?,?,?,?,?,?,?,?)",(
 			self.firstname,
@@ -28,35 +33,20 @@ class Student():
 			self.year,
 			self.course	
 		))
-		return print(f'{self.fullname} successfully created as a student...')
-
-
-	def add_course(self,student,course,yr):
-		student_tuple,course_tuple = (student,),(course_tuple,)
-		if student_tuple not in self.cursr.execute("SELECT fullname FROM students") or course_tuple not in self.cursr.execute("SELECT name FROM courses"):
-			return print('Error 404')
 
 		# configuring objects
-		course_id = f'{course}/yr-{yr}'
 		course_students = self.cursr.execute("SELECT students FROM courses WHERE courseId = ?",(course_id,)).fetchone()
 		cs_list = json.loads(course_students[0])
-
-		# checking the student
-		if student in cs_list or self.cursr.execute("SELECT course FROM students WHERE fullname = ?",(student,)).fetchone()[0] != None:
-			return print('Student is already enrolled...')
 
 		# updating the server
 		cs_list.append(student)
 		self.cursr.execute("UPDATE courses SET students = ? WHERE courseId = ?",(json.dumps(cs_list),course_id))
-		self.cursr.execute("UPDATE students SET course = ? WHERE fullname =?",(course,student))
 
-		# committing and closing
 		self.cnt.commit()
 		self.cnt.close()
-
-		return print(f'{student} enrolled in {course}-{yr} successfully')
-
+		
+		return print(f'{self.fullname} successfully created as a student...')
 
 
 if __name__ == '__main__':
-	pass
+	s1 = Student().create_student('Michael', )
